75e852ac321b90620d30cec23e7dae1bc4c7ed5f
utkarshrajputt <utkarshrajput1583@gmail.com>
2025-07-01 15:55:55 +0530

    changes for render deployment

--- QuickTalk/settings.py
+++ QuickTalk/settings.py
@@ -77,6 +77,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
